ef44db2ecbaf2abd59add19e4e4e48d95d5af3a3
Ibnballo1 <belloabdullateef035@gmail.com>
2022-07-06 16:20:22 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Calculator from './components/Calculator';
 function App() {
   return (
     <div className="App">
-      <Calculator my-props="t" />
+      <Calculator />
     </div>
   );
 }
