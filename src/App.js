ce474fcc3dab465b43dd6393fb0d074263c7ac5b
Ibnballo1 <belloabdullateef035@gmail.com>
2022-07-06 16:51:25 +0100

    Change function component to class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable linebreak-style */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable linebreak-style */
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
